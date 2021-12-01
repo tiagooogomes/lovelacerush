adc9743f9febd422261aea27cb5d7d26d8850354
tiagooogomes <91339622+tiagooogomes@users.noreply.github.com>
2021-12-01 10:28:22 -0300

    Final

--- script.js
+++ script.js
@@ -17,28 +17,20 @@ function menosRodada(){
         console.log(`Carro da Edna ${voltaEdna}`);
 
         if(voltaPedro > voltaJuca && voltaPedro > voltaEdna){
-            console.log("Pedro Ganhou");
             totalPedro = totalPedro + 1;
         }else if(voltaJuca > voltaPedro && voltaJuca > voltaEdna){
-            console.log("Juca Ganhou");
             totalJuca = totalJuca + 1;
         }else if(voltaEdna > voltaPedro && voltaEdna > voltaJuca){
-            console.log("Edna Ganhou");
             totalEdna = totalEdna + 1;
         }
 
-
         if(i == 10){
 
-            console.log("TESTE")
             if(totalPedro > totalJuca && totalPedro > totalEdna){
-                console.log("PEDRO GANHOU");
                 texto.innerHTML = "Ninguém segura o PEDRO";
             }else if(totalJuca > totalPedro && totalJuca > totalEdna){
-                console.log("JUCA GANHOU");
                 texto.innerHTML = "Isso é mamão com açucar para o JUCA";
             }else if(totalEdna > totalPedro && totalEdna > totalJuca){
-                console.log("EDNA GANHOU")
                 texto.innerHTML = "EDNA está botando pra quebrar";
             }
         }
@@ -60,33 +52,21 @@ function mediaRodada(){
         let voltaJuca = random(120, 260) * 0.95;
         let voltaEdna = random(180, 220) * 0.99;
 
-        console.log(`Carro do Pedro ${voltaPedro}`);
-        console.log(`Carro do Juca ${voltaJuca}`);
-        console.log(`Carro da Edna ${voltaEdna}`);
-
         if(voltaPedro > voltaJuca && voltaPedro > voltaEdna){
-            console.log("Pedro Ganhou");
             totalPedro = totalPedro + 1;
         }else if(voltaJuca > voltaPedro && voltaJuca > voltaEdna){
-            console.log("Juca Ganhou");
             totalJuca = totalJuca + 1;
         }else if(voltaEdna > voltaPedro && voltaEdna > voltaJuca){
-            console.log("Edna Ganhou");
             totalEdna = totalEdna + 1;
         }
 
-
         if(i == 70){
 
-            console.log("TESTE")
             if(totalPedro > totalJuca && totalPedro > totalEdna){
-                console.log("PEDRO GANHOU");
                 texto.innerHTML = "Pedro não está para brincadeira";
             }else if(totalJuca > totalPedro && totalJuca > totalEdna){
-                console.log("JUCA GANHOU");
                 texto.innerHTML = "Para o JUCA, vencer é uma obrigação";
             }else if(totalEdna > totalPedro && totalEdna > totalJuca){
-                console.log("EDNA GANHOU")
                 texto.innerHTML = "EDNA se tornou lendária";
             }
         }
@@ -108,32 +88,21 @@ function maximaRodada(){
         let voltaJuca = random(120, 260) * 0.95;
         let voltaEdna = random(180, 220) * 0.99;
 
-        console.log(`Carro do Pedro ${voltaPedro}`);
-        console.log(`Carro do Juca ${voltaJuca}`);
-        console.log(`Carro da Edna ${voltaEdna}`);
-
         if(voltaPedro > voltaJuca && voltaPedro > voltaEdna){
-            console.log("Pedro Ganhou");
             totalPedro = totalPedro + 1;
         }else if(voltaJuca > voltaPedro && voltaJuca > voltaEdna){
-            console.log("Juca Ganhou");
             totalJuca = totalJuca + 1;
         }else if(voltaEdna > voltaPedro && voltaEdna > voltaJuca){
-            console.log("Edna Ganhou");
             totalEdna = totalEdna + 1;
         }
 
         if(i == 160){
 
-            console.log("TESTE")
             if(totalPedro > totalJuca && totalPedro > totalEdna){
-                console.log("PEDRO GANHOU");
                 texto.innerHTML = "Se tiver PEDRO como adversario, desista";
             }else if(totalJuca > totalPedro && totalJuca > totalEdna){
-                console.log("JUCA GANHOU");
                 texto.innerHTML = "JUCA está viciado na linha de chegada";
             }else if(totalEdna > totalPedro && totalEdna > totalJuca){
-                console.log("EDNA GANHOU")
                 texto.innerHTML = "É o superman? O batman? A não, é a EDNA";
             }
         }
